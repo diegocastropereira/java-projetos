055b5c7ac950ff395346abe300b513a79bb45630
diegocastropereira <diegocastro10032004@gmail.com>
2023-09-05 20:45:44 -0400

    programa com as funcoes basicas

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia, nomeCliente;
+        float saldo;
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.printf("Digite o número da sua conta: ");
+            numero = input.nextInt();
+
+            System.out.printf("Digite o número da sua agencia: ");
+            agencia = input.next();
+
+            System.out.printf("Digite seu nome: ");
+            nomeCliente = input.next();
+
+            System.out.printf("Digite seu saldo: ");
+            saldo = input.nextFloat();
+        }
+
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo de "+ saldo +" já está disponível para saque");
     }
 }
